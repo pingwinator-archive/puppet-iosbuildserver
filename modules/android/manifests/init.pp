acd6f2c3636405652073205e7e0be36e026437ea
Vasyl Liutikov <vasya.lutikov@postindustria.com>
2013-02-27 16:02:02 +0200

    next version for android

--- modules/android/manifests/init.pp
+++ modules/android/manifests/init.pp
@@ -1,11 +1,22 @@
-class android {
-  staging::file { 'android-sdk-mac.zip':
-  source => 'http://192.168.100.8/xcode/xcode4.6/android-sdk-mac.zip'
-}
+class android { 
+  include wget
+   $installdir = '/Developer/android'
+   $source  = "http://192.168.100.8/xcode/xcode4.6/android-sdk-mac.zip"
+   $archive = "${installdir}/android-sdk-mac.zip"
 
-staging::extract { 'android-sdk-mac.zip':
-  target  => '/Developer/',
-  creates => '/Developer/android',
-  require => Staging::File['android-sdk-mac.zip'],
-}
+
+
+  file { $installdir:
+    ensure => directory,
+  } ->
+  wget::fetch { 'download-androidsdk':
+    source      => $source,
+    destination => $archive,
+  } ->
+  exec { 'unpack-androidsdk':
+    command => '/usr/bin/unzip ${archive}',
+    creates => $installdir,
+    cwd     => $installdir
+  }
+
 }
