356b0bd60c3d2934ac61e97d20aa1c7583e83960
Vasyl Liutikov <vasya.lutikov@postindustria.com>
2013-02-27 11:50:19 +0200

    added jdk installation

--- modules/java/manifests/init.pp
+++ modules/java/manifests/init.pp
@@ -1,6 +1,17 @@
 class java {
-  package { 'Java':
-    provider => 'pkgdmg',
-    source   => 'http://192.168.100.8/xcode/xcode4.6/jre-7u15-macosx-x64.dmg'
+  $jre_url = 'http://192.168.100.8/xcode/xcode4.6/jre-7u15-macosx-x64.dmg'
+  $jdk_url = 'http://192.168.100.8/xcode/xcode4.6/jdk-7u15-macosx-x64.dmg'
+
+  package {
+    'jre-7u15.dmg':
+      ensure   => present,
+      alias    => 'java-jre',
+      provider => pkgdmg,
+      source   => $jre_url ;
+    'jdk-7u15.dmg':
+      ensure   => present,
+      alias    => 'java',
+      provider => pkgdmg,
+      source   => $jdk_url ;
   }
 }
